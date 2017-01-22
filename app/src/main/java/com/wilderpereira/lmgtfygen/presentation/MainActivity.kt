2894ada3c9e2eb64cad4009809b2fc2fc37020a9
Wilder Pereira <wilder_roberto@hotmail.com>
2017-01-22 15:58:07 -0200

    Added method to load spinner items

--- app/src/main/java/com/wilderpereira/lmgtfygen/presentation/MainActivity.kt
+++ app/src/main/java/com/wilderpereira/lmgtfygen/presentation/MainActivity.kt
@@ -2,14 +2,26 @@ package com.wilderpereira.lmgtfygen.presentation
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import com.jakewharton.rxbinding.widget.RxAdapterView
 import com.wilderpereira.lmgtfygen.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadSpinner()
+    }
+
+    private fun loadSpinner(){
+        val adapter = ArrayAdapter.createFromResource(
+                this, R.array.search_types, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        searchTypeSpinner.adapter = adapter
     }
 
 }
