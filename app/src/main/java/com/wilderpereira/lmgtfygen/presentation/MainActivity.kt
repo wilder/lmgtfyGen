b9c9de8e445f968fbb76ffe8f2f6a91c28a802af
Wilder Pereira <wilder_roberto@hotmail.com>
2017-08-29 00:51:34 -0300

    fix RateThisApp

--- app/src/main/java/com/wilderpereira/lmgtfygen/presentation/MainActivity.kt
+++ app/src/main/java/com/wilderpereira/lmgtfygen/presentation/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
         val config = RateThisApp.Config(3, 5)
         RateThisApp.init(config)
+        RateThisApp.onCreate(this@MainActivity)
 
         loadSpinner()
 
